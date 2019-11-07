c6f6bccd637a43a036e879a6205230de0eb400db
dengzhiqiang <zhiqiang.deng@fft.com.cn>
2019-11-07 11:34:42 +0800

    [k-vue] 编译指令

--- KVue/compile.js
+++ KVue/compile.js
@@ -35,6 +35,21 @@ class Compile {
             // 类型判断
             if (this.isElement(node)) {
                 // 元素
+                const nodeAttrs = node.attributes;
+                Array.from(nodeAttrs).forEach(attr => {
+                    const attrName = attr.name; //属性名
+                    const exp = attr.value; // 属性值
+                    if (this.isDirective(attrName)) {
+                        // k-text
+                        const dir = attrName.substring(2);
+                        // 执行指令
+                        this[dir] && this[dir](node, this.$vm, exp);
+                    }
+                    if (this.isEvent(attrName)) {
+                        const dir = attrName.substring(1); // @click
+                        this.eventHandler(node, this.$vm, exp, dir);
+                    }
+                });
             }else if (this.isInterpolation(node)) {
                 // 文本
                 this.compileText(node);
@@ -51,6 +66,21 @@ class Compile {
         node.textContent = this.$vm.$data[RegExp.$1];
     }
 
+    // 事件处理器
+    eventHandler(node, vm, exp, dir) {
+        // @click="onClick"
+        let fn = vm.$options.methods && vm.$options.methods[exp];
+        if (dir && fn) {
+            node.addEventListener(dir, fn.bind(vm));
+        }
+    }
+
+    isDirective(attr) {
+        return attr.indexOf("k-") == 0;
+    }
+    isEvent(attr) {
+        return attr.indexOf("@") == 0;
+    }
     isElement(node) {
         return node.nodeType === 1;
     }
