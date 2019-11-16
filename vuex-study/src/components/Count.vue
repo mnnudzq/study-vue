0c804ef5813f4bf806e5fdd318f32913453bbd70
dengzhiqiang <zhiqiang.deng@fft.com.cn>
2019-11-16 15:43:05 +0800

    [demo] vuex-简单用法

---
+++ vuex-study/src/components/Count.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="count">
+      {{count}}
+      <button @click="$store.commit('add')">+</button>
+      <button @click="$store.commit('reduce')">-</button>
+      {{$store.state.count}}
+    </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+    export default {
+        name: "Count",
+        data() {
+            return {
+                // count: null
+            }
+        },
+        // 写法1
+        // computed: {
+        //     count() {
+        //         return this.$store.state.count;
+        //     }
+        // },
+        // 写法2
+        // computed: mapState({
+        //     count: state => state.count
+        //     // count(state) {
+        //     //     return state.count;
+        //     // }
+        // }),
+        // 写法3
+        // computed: mapState(['count']),
+        // 写法4
+        computed: {
+            // es6解构
+            ...mapState(['count'])
+        },
+        methods: {
+            // test() {
+            //     this.count = this.$store.commit('add');
+            //     // this.$store.state.count = count;
+            // }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- vuex-study/src/vuex/store.js
+++ vuex-study/src/vuex/store.js
@@ -1,5 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
-export default new Vuex.Store({})
+Vue.use(Vuex);
+
+const state = {
+  count: 1
+}
+
+const mutations = {
+  add(state) {
+    state.count ++;
+    return state.count;
+  },
+  reduce(state) {
+    state.count --;
+    return state.count;
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
